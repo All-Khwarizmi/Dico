bce8ab7d47699a8c0a372faed85a8b9d4188f15f
Jason Suarez <swarecito@gmail.com>
2024-02-01 06:20:56 +0100

    Update CORS headers in next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@
 const getCorsHeaders = () => {
   const headers = {};
 
-  headers["Access-Control-Allow-Origin"] = "http://localhost:1420";
+  headers["Access-Control-Allow-Origin"] = [
+    "http://localhost:3000",
+    "https://dico-uno.vercel.app",
+  ];
   headers["Access-Control-Allow-Credentials"] = "true";
   headers["Access-Control-Allow-Methods"] = "GET,OPTIONS,PATCH,DELETE,POST,PUT";
   headers["Access-Control-Allow-Headers"] =
