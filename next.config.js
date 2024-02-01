d656f7953412d52810d3ea16566d14bc4b679583
Jason Suarez <swarecito@gmail.com>
2024-02-01 06:11:49 +0100

    add cors headers to next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,30 @@
 /** @type {import('next').NextConfig} */
+
+const getCorsHeaders = () => {
+  const headers = {};
+
+  headers["Access-Control-Allow-Origin"] = "http://localhost:1420";
+  headers["Access-Control-Allow-Credentials"] = "true";
+  headers["Access-Control-Allow-Methods"] = "GET,OPTIONS,PATCH,DELETE,POST,PUT";
+  headers["Access-Control-Allow-Headers"] =
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization";
+
+  return Object.entries(headers).map(([key, value]) => ({ key, value }));
+};
+
 const nextConfig = {
   experimental: {
     appDir: true,
   },
+  headers: async () => {
+    return [
+      {
+        source: "/(.*)",
+        headers: getCorsHeaders(),
+      },
+    ];
+  },
+
   eslint: {
     // Warning: If set to true this allows production builds to successfully complete even if
     // your project has ESLint errors.
@@ -10,4 +32,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
