9179d28d088e2fb3348c6d13ce9e5f93fc34ed06
Jason Suarez <swarecito@gmail.com>
2024-03-09 21:55:00 +0100

    Add queryPonsApi import

--- src/pages/api/translations.ts
+++ src/pages/api/translations.ts
@@ -3,6 +3,7 @@ import { Payload, payloadSchema } from "../../utils/schemas/payload";
 import prisma from "../../utils/db";
 import { Trad } from "../../utils/types";
 import { Word } from "@prisma/client";
+import { queryPonsApi } from "@/utils/queryPonsApi";
 
 export default async function handler(
   req: NextApiRequest,
@@ -99,32 +100,3 @@ export default async function handler(
   }
 }
 
-/**
- * Builds the URL for the translation API based on the given word and source language.
- * @param word - The word to be translated.
- * @param source - The source language of the word. Can be either "fr" for French or "es" for Spanish.
- * @returns The URL for the translation API.
- */
-export function urlBuilder(word: string, source: "fr" | "es") {
-  const direction = source === "fr" ? "es" : "fr";
-  return `https://api.pons.com/v1/dictionary?q=${word}&in=${source}&language=${direction}&l=esfr`;
-}
-
-/**
- * Queries the PONS API to get translations for a given word.
- *
- * @param source - The source language of the word (either "fr" for French or "es" for Spanish).
- * @param word - The word to be translated.
- * @returns A Promise that resolves to the response from the PONS API.
- */
-export function queryPonsApi(source: "fr" | "es", word: string) {
-  const url = urlBuilder(word, source);
-  const options: RequestInit = {
-    method: "GET",
-    headers: {
-      "X-secret": process.env.NEXT_PUBLIC_SECRET!,
-    },
-  };
-
-  return fetch(url, options);
-}
