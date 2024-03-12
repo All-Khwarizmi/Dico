4895acf97bcccfbfc75501d5656b5c24e75c9467
Jason Suarez <swarecito@gmail.com>
2024-03-12 16:34:47 +0100

    Refactor search test in search.spec.ts

--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -6,21 +6,36 @@ test.describe("Page is available", () => {
 
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/Dico/);
+
+    test("Should allow to search words", async ({ page }) => {
+
+      await expect(
+        page.getByPlaceholder("Que veux-tu chercher?")
+      ).toBeVisible();
+      const input = page.getByPlaceholder("Que veux-tu chercher?");
+
+      const submitButton = page.getByText("Chercher");
+
+      await input.fill("casa");
+      await submitButton.click();
+
+      await expect(page.getByTestId("maison ,[object Object]")).toBeVisible();
+    });
   });
 });
 
-test.describe("Search a new word", () => {
-  test("Should allow to search words", async ({ page }) => {
-    await page.goto("http://localhost:3000/");
+// test.describe("Search a new word", () => {
+//   test("Should allow to search words", async ({ page }) => {
+//     await page.goto("http://localhost:3000/");
 
-    await expect(page.getByPlaceholder("Que veux-tu chercher?")).toBeVisible();
-    const input = page.getByPlaceholder("Que veux-tu chercher?");
+//     await expect(page.getByPlaceholder("Que veux-tu chercher?")).toBeVisible();
+//     const input = page.getByPlaceholder("Que veux-tu chercher?");
 
-    const submitButton = page.getByText("Chercher");
+//     const submitButton = page.getByText("Chercher");
 
-    await input.fill("casa");
-    await submitButton.click();
+//     await input.fill("casa");
+//     await submitButton.click();
 
-    await expect(page.getByTestId("maison ,[object Object]")).toBeVisible();
-  });
-});
+//     await expect(page.getByTestId("maison ,[object Object]")).toBeVisible();
+//   });
+// });
