a71ebe42019739577dbc7565627840fc830495ec
Jason Suarez <swarecito@gmail.com>
2024-03-12 10:51:47 +0100

    Add tests for page title and word search functionality

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,37 @@
 import { test, expect } from "@playwright/test";
 
-test("has title", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:3000/");
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Dico/);
 });
 
 test("get started link", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
-
   // // Click the get started link.
   // await page.getByRole('main', { name: 'container' }).click();
-
   // // Expects page to have a heading with the name of Installation.
   // await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+
+test.describe("Page is available", () => {
+  test("has title", async ({ page }) => {
+    await page.goto("http://localhost:3000/");
+
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Dico/);
+  });
+});
+
+test.describe("Search a new word", () => {
+  test("Should allow to search words", async ({ page }) => {
+    await page.goto("http://localhost:3000/");
+
+    const input = page.getByPlaceholder("Que veux-tu chercher?");
+    const submitButton = page.getByRole("button", { name: "Chercher" });
+
+    await input.fill("casa");
+    await submitButton.click();
+
+    const word = page.getByTestId("maison ,[object Object]");
+
+    await expect(word).toBeVisible();
+  });
 });
