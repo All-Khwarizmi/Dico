3ffdfa98f5660d9e5a04aab505b0250629334195
Jason Suarez <swarecito@gmail.com>
2024-03-12 16:46:00 +0100

    Refactor search test in search.spec.ts

--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -7,20 +7,15 @@ test.describe("Page is available", () => {
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/Dico/);
 
-    test("Should allow to search words", async ({ page }) => {
+    await expect(page.getByPlaceholder("Que veux-tu chercher?")).toBeVisible();
+    const input = page.getByPlaceholder("Que veux-tu chercher?");
 
-      await expect(
-        page.getByPlaceholder("Que veux-tu chercher?")
-      ).toBeVisible();
-      const input = page.getByPlaceholder("Que veux-tu chercher?");
+    const submitButton = page.getByText("Chercher");
 
-      const submitButton = page.getByText("Chercher");
+    await input.fill("casa");
+    await submitButton.click();
 
-      await input.fill("casa");
-      await submitButton.click();
-
-      await expect(page.getByTestId("maison ,[object Object]")).toBeVisible();
-    });
+    await expect(page.getByTestId("maison ,[object Object]")).toBeVisible();
   });
 });
 
