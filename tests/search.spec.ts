d5425197c5ab3c213fa4a78d28cf88c4a7f234a0
Jason Suarez <swarecito@gmail.com>
2024-03-13 07:05:44 +0100

    Fix URLs in test file

--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 
 test.describe("Page is available", () => {
   test("has title", async ({ page }) => {
-    await page.goto("http://localhost:3000/");
+    await page.goto("http://localhost:3000");
 
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/Dico/);
@@ -11,7 +11,7 @@ test.describe("Page is available", () => {
 
 test.describe("Search a new word", () => {
   test("Should allow to search words", async ({ page }) => {
-    await page.goto("http://localhost:3000/");
+    await page.goto("http://localhost:3000");
 
     await expect(page.getByPlaceholder("Que veux-tu chercher?")).toBeVisible();
     const input = page.getByPlaceholder("Que veux-tu chercher?");
