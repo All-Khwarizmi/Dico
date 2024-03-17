2726daff76f1191ab5e3e2578bd7b815d331c52f
Jason Suarez <swarecito@gmail.com>
2024-03-17 13:38:40 +0100

    Add rendering of search input in App component

--- src/app/app.spec.tsx
+++ src/app/app.spec.tsx
@@ -3,9 +3,15 @@ import App from "/Users/jasonsuarez/App/dico-local/src/app/page";
 import { render, screen } from "@testing-library/react";
 
 describe("App", () => {
+  render(<App />);
+
   it("Should render the title", () => {
-    render(<App />);
     const title = screen.getByText("Dico");
     expect(title).toBeDefined();
+  });
+
+  it("Should render the search input", () => {
+    const input = screen.getByPlaceholderText("Que veux-tu chercher?");
+    expect(input).toBeDefined();
   });
 });
