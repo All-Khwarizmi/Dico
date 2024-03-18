073897cbdef395e02b69fb9168003468f939f363
Jason Suarez <swarecito@gmail.com>
2024-03-18 14:21:13 +0100

    📦 NEW: Add stats flow from api to new page with data fetching with react query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import theme from "@/styles/theme";
 import Main from "@/components/Main";
 
+
 export default function App() {
   return (
     <>
---
+++ src/app/stats/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import WordsList from "@/components/WordsList"; // Ajustez le chemin d'importation selon votre structure
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Popular Words</h1>
+      <WordsList />
+    </div>
+  );
+}
