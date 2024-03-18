494be04d5c49cc975af32beb4da622b226944713
Jason Suarez <swarecito@gmail.com>
2024-03-18 22:03:45 +0100

    👌 IMPROVE: Add usePathName hook and refactor header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,21 @@
+"use client";
+import usePathName from "@/hooks/usePathName";
 import { HomeIcon, MoonIcon, StatsIcon, SunIcon } from "@/utils/icons/icons";
 import { useColorMode } from "@chakra-ui/react";
-import Stats from "./stats/Stats";
 
 export default function Header() {
   const { colorMode, toggleColorMode } = useColorMode();
-  const pathName = window.location.pathname;
+  const path = usePathName();
 
   return (
     <header className="absolute top-0 right-0  p-5 flex justify-center items-center gap-5">
       <a
-        href={`${pathName === "/stats" ? "/" : "/stats"}`}
+        href={`${path === "/stats" ? "/" : "/stats"}`}
         className={`text-lg ${
           colorMode === "light" ? "text-black" : "text-white"
         }`}
       >
-        {pathName === "/stats" ? <HomeIcon /> : <StatsIcon />}
+        {path === "/stats" ? <HomeIcon /> : <StatsIcon />}
       </a>
       <button className="text-lg" onClick={() => toggleColorMode()}>
         {colorMode === "light" ? <SunIcon /> : <MoonIcon />}
--- src/components/stats/Stats.tsx
+++ src/components/stats/Stats.tsx
@@ -1,8 +1,9 @@
+import { icons } from "react-icons/lib";
 import WordsList from "./WordsList";
 
 export default function Stats() {
   return (
-    <main className="mx-auto h-full flex flex-col md:flex-row justify-center">
+    <main className="mx-auto h-full flex gap-4 flex-col md:flex-row justify-center">
       <h1 className="text-3xl">Stats</h1>
       <WordsList></WordsList>
     </main>
