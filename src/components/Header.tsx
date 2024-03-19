856fc1eba04471e7092d6a10ee786685c0d6c48b
Jason Suarez <swarecito@gmail.com>
2024-03-19 10:33:13 +0100

    👌 IMPROVE: Add conditinal rendering to navigation links and accessibility

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,21 +3,32 @@ import usePathName from "@/hooks/usePathName";
 import { HomeIcon, MoonIcon, StatsIcon, SunIcon } from "@/utils/icons/icons";
 import { useColorMode } from "@chakra-ui/react";
 
+export function useDevMode() {
+  return process.env.NODE_ENV === "development";
+}
 export default function Header() {
   const { colorMode, toggleColorMode } = useColorMode();
   const path = usePathName();
+  const devMode = useDevMode();
 
   return (
     <header className="absolute top-0 right-0  p-5 flex justify-center items-center gap-5">
-      <a
-        href={`${path === "/stats" ? "/" : "/stats"}`}
-        className={`text-lg ${
-          colorMode === "light" ? "text-black" : "text-white"
-        }`}
+      {devMode && (
+        <a
+          aria-label={path === "/stats" ? "Home" : "Stat's Page"}
+          href={`${path === "/stats" ? "/" : "/stats"}`}
+          className={`text-lg ${
+            colorMode === "light" ? "text-black" : "text-white"
+          }`}
+        >
+          {path === "/stats" ? <HomeIcon /> : <StatsIcon />}
+        </a>
+      )}
+      <button
+        aria-label="Toggle color mode"
+        className="text-lg"
+        onClick={() => toggleColorMode()}
       >
-        {path === "/stats" ? <HomeIcon /> : <StatsIcon />}
-      </a>
-      <button className="text-lg" onClick={() => toggleColorMode()}>
         {colorMode === "light" ? <SunIcon /> : <MoonIcon />}
       </button>
     </header>
