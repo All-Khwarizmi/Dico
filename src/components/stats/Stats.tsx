72763243459f3d7fe81042732ee89a860d2faec6
Jason Suarez <swarecito@gmail.com>
2024-03-19 10:27:35 +0100

    📦 NEW: Add react charts and make some simple UI to display the top searches section

--- src/components/stats/Stats.tsx
+++ src/components/stats/Stats.tsx
@@ -1,11 +1,14 @@
-import { icons } from "react-icons/lib";
 import WordsList from "./WordsList";
 
 export default function Stats() {
   return (
-    <main className="pt-24 mx-auto h-full w-full flex gap-4 flex-col md:flex-row justify-start">
-      <h1 className="text-3xl">Stats</h1>
-      <WordsList></WordsList>
+    <main className="pt-24 mx-auto h-full w-full justify-start">
+      <header className="flex flex-col gap-4 gap-y-12 pb-10">
+        <h1 className="text-5xl">Dico en nombres</h1>
+      </header>
+      <section className="w-full flex gap-4 gap-y-8 flex-col md:flex-row px-4">
+        <WordsList />
+      </section>
     </main>
   );
 }
--- src/components/stats/WordsList.tsx
+++ src/components/stats/WordsList.tsx
@@ -1,4 +1,7 @@
 import { useQuery, useQueryClient } from "@tanstack/react-query";
+import React from "react";
+import { AxisOptions, Chart } from "react-charts";
+
 async function fetchWords() {
   const response = await fetch("/api/words");
   if (!response.ok) {
@@ -15,6 +18,53 @@ interface WordSearches {
 export default function WordsList() {
   const queryClient = useQueryClient();
   const query = useQuery({ queryKey: ["words"], queryFn: fetchWords });
+  const primaryAxis = React.useMemo(
+    (): AxisOptions<WordSearches> => ({
+      getValue: (word) => word.word,
+    }),
+    []
+  );
 
-  return <></>;
+  const secondaryAxes = React.useMemo(
+    (): AxisOptions<WordSearches>[] => [
+      {
+        getValue: (word) => word.searches,
+      },
+    ],
+    []
+  );
+  const data = [
+    {
+      label: "Words",
+      data: query.data?.map((word: WordSearches) => {
+        return {
+          word: word.word,
+          searches: word.searches,
+        };
+      }),
+    },
+  ];
+  if (query.isError) {
+    return <div>Failed to load data</div>;
+  }
+  if (query.isLoading) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <article className="w-full h-full">
+      <h2 className="text-2xl text-center pb-4">
+        Les mots les plus recherchés
+      </h2>
+      <div className="w-full h-96">
+        <Chart
+          className="w-full h-full bg-white"
+          options={{
+            data,
+            primaryAxis,
+            secondaryAxes,
+          }}
+        />
+      </div>
+    </article>
+  );
 }
